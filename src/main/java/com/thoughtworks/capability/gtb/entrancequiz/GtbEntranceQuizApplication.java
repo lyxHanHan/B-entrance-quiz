4f064803b064a2250917c7b570ec5381d6b7e8f6
Willie Zhang <wwdzhang@thoughtworks.com>
2020-11-03 19:20:51 +0800

    [wzhang]add code comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -9,5 +9,5 @@ public class GtbEntranceQuizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GtbEntranceQuizApplication.class, args);
 	}
-
+	//TODO GTB-综合: * 目前代码无法正常编译，建议先按照自己的思路，对需求进行拆分，逐步实现一次
 }
